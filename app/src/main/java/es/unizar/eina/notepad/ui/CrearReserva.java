4b0ba742f6ec1bfeb5a5aefa62c8d40c08b79f7a
Simon30804 <869800@unizar.es>
2024-10-31 13:56:16 +0100

    Cambios para arreglar varios aspectos

--- app/src/main/java/es/unizar/eina/notepad/ui/CrearReserva.java
+++ app/src/main/java/es/unizar/eina/notepad/ui/CrearReserva.java
@@ -1,85 +1,72 @@
 package es.unizar.eina.notepad.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import es.unizar.eina.notepad.R;
 
-/** Pantalla utilizada para la creación o edición de una reserva */
+/** Pantalla utilizada para la creación de una nueva reserva */
 
 public class CrearReserva extends AppCompatActivity {
 
-    public static final String RESERVA_FECHAINICIO = "fecha_inicio";
-    public static final String RESERVA_FECHAFIN = "fecha_fin";
-    public static final String RESERVA_NOMBRECLIENTE = "nombre_cliente";
+    public static final String RESERVA_FECHA_INICIO = "fecha_inicio";
+    public static final String RESERVA_FECHA_FIN = "fecha_fin";
+    public static final String RESERVA_NOMBRE_CLIENTE = "nombre_cliente";
     public static final String RESERVA_TELEFONO = "telefono";
-    public static final String RESERVA_NUMOCUPANTES = "numOcupantes";
-    public static final String RESERVA_ID = "id";
+    public static final String RESERVA_NUM_OCUPANTES = "num_ocupantes";
 
     private EditText mFechaInicioText;
-
     private EditText mFechaFinText;
-
     private EditText mNombreClienteText;
-
     private EditText mTelefonoText;
-
     private EditText mNumOcupantesText;
-
-    private Integer mRowId;
-
-    Button mSaveButton;
+    private Button mAceptarButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crearreserva);
+        setContentView(R.layout.activity_crear_reserva);
 
+        // Enlazamos los elementos de la interfaz
         mFechaInicioText = findViewById(R.id.fecha_inicio);
         mFechaFinText = findViewById(R.id.fecha_fin);
         mNombreClienteText = findViewById(R.id.nombre_cliente);
         mTelefonoText = findViewById(R.id.telefono);
-        mNumOcupantesText = findViewById(R.id.numOcupantes);
+        mNumOcupantesText = findViewById(R.id.num_ocupantes);
+        mAceptarButton = findViewById(R.id.button_save);
 
-        mSaveButton = findViewById(R.id.button_save);
+        // Configuramos la acción del botón "Guardar"
+        mAceptarButton.setOnClickListener(view -> {
+            if (areFieldsValid()) {
+                Intent replyIntent = new Intent();
+                replyIntent.putExtra(RESERVA_FECHA_INICIO, mFechaInicioText.getText().toString());
+                replyIntent.putExtra(RESERVA_FECHA_FIN, mFechaFinText.getText().toString());
+                replyIntent.putExtra(RESERVA_NOMBRE_CLIENTE, mNombreClienteText.getText().toString());
+                replyIntent.putExtra(RESERVA_TELEFONO, mTelefonoText.getText().toString());
+                replyIntent.putExtra(RESERVA_NUM_OCUPANTES, Integer.parseInt(mNumOcupantesText.getText().toString()));
 
-        mSaveButton.setOnClickListener(view -> {
-            Intent replyIntent = new Intent();
-            if (TextUtils.isEmpty(mFechaInicioText.getText())) {
-                setResult(RESULT_CANCELED, replyIntent);
-                Toast.makeText(getApplicationContext(), R.string.empty_not_saved, Toast.LENGTH_LONG).show();
-            } else {
-                replyIntent.putExtra(CrearReserva.RESERVA_FECHAINICIO, mFechaInicioText.getText().toString());
-                replyIntent.putExtra(CrearReserva.RESERVA_FECHAFIN, mFechaFinText.getText().toString());
-                replyIntent.putExtra(CrearReserva.RESERVA_NOMBRECLIENTE, mNombreClienteText.getText().toString());
-                replyIntent.putExtra(CrearReserva.RESERVA_TELEFONO, mTelefonoText.getText().toString());
-                replyIntent.putExtra(CrearReserva.RESERVA_NUMOCUPANTES, mNumOcupantesText.getText().toString());
-                if (mRowId!=null) {
-                    replyIntent.putExtra(CrearReserva.RESERVA_ID, mRowId.intValue());
-                }
                 setResult(RESULT_OK, replyIntent);
+                finish();
+            } else {
+                Toast.makeText(this, R.string.empty_not_saved, Toast.LENGTH_LONG).show();
             }
-            finish();
         });
     }
 
-    private void populateFields () {
-        mRowId = null;
-        Bundle extras = getIntent().getExtras();
-        if (extras!=null) {
-            mFechaInicioText.setText(extras.getString(CrearReserva.RESERVA_FECHAINICIO));
-            mFechaFinText.setText(extras.getString(CrearReserva.RESERVA_FECHAFIN));
-            mNombreClienteText.setText(extras.getString(CrearReserva.RESERVA_NOMBRECLIENTE));
-            mTelefonoText.setText(extras.getString(CrearReserva.RESERVA_TELEFONO));
-            mNumOcupantesText.setText(extras.getString(CrearReserva.RESERVA_NUMOCUPANTES));
-            mRowId = extras.getInt(CrearReserva.RESERVA_ID);
-        }
+    /**
+     * Método para validar que los campos requeridos no estén vacíos.
+     *
+     * @return true si todos los campos son válidos, false en caso contrario.
+     */
+    private boolean areFieldsValid() {
+        return !TextUtils.isEmpty(mFechaInicioText.getText()) &&
+                !TextUtils.isEmpty(mFechaFinText.getText()) &&
+                !TextUtils.isEmpty(mNombreClienteText.getText()) &&
+                !TextUtils.isEmpty(mTelefonoText.getText()) &&
+                !TextUtils.isEmpty(mNumOcupantesText.getText());
     }
-
 }
